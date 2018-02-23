81b758efb63f8b05560f84826d834e3002472b1d
Alexander <dev@salex.pro>
2018-02-23 16:47:34 +0200

    countdown

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,5 @@
-/* 
+/* global moment */
+/*
 @codekit-prepend '../../node_modules/jquery/dist/jquery.min';
 @codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min';
 @codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min';
@@ -12,13 +13,15 @@
 @codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.slider.min';
 @codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.accordion.min';
 @codekit-prepend './lib/jquery-select7';
+@codekit-prepend '../../node_modules/moment/moment';
+@codekit-prepend '../../node_modules/moment-timezone/builds/moment-timezone-with-data-2012-2022.min';
+@codekit-prepend '../../node_modules/jquery-countdown/dist/jquery.countdown.min';
 @codekit-prepend '../../node_modules/owl.carousel/dist/owl.carousel.min';
 */
 
 $(document).foundation();
 
 $('.select7').select7();
-
 
 // Fill top bar
 const top_fill = () => {
@@ -34,6 +37,17 @@ top_fill();
 
 $(document).scroll(() => {
     top_fill();
+});
+
+// Countdown
+var hourX = moment.tz('2018-03-01 00:00', 'America/Sao_Paulo');
+
+$('.countdown_container').countdown(hourX.toDate(), function(event) {
+    $(this).html(event.strftime(''
+        + '<div class="countdown_digit"><div class="countdown_digit_num">%d</div><div class="countdown_digit_label">Days</div></div><div class="countdown_sep">:</div>'
+        + '<div class="countdown_digit"><div class="countdown_digit_num">%H</div><div class="countdown_digit_label">Hours</div></div><div class="countdown_sep">:</div>'
+        + '<div class="countdown_digit"><div class="countdown_digit_num">%M</div><div class="countdown_digit_label">Minutes</div></div><div class="countdown_sep">:</div>'
+        + '<div class="countdown_digit"><div class="countdown_digit_num">%S</div><div class="countdown_digit_label">Seconds</div></div>'));
 });
 
 // Graph slider
