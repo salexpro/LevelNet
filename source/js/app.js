9725570cd1d11560cacc309d61f7df5003f4b190
Alexander <dev@salex.pro>
2018-03-06 15:44:23 +0200

    share button links & call me back

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,5 @@
 /* global sr, ScrollReveal, moment, intercomSettings */
-/* eslint no-inner-declarations: 'off' */
+/* eslint no-inner-declarations: 'off', no-unused-vars: 'off' */
 /*
 @codekit-prepend '../../node_modules/jquery/dist/jquery.min';
 @codekit-prepend '../../node_modules/scrollreveal/dist/scrollreveal.min';
@@ -317,3 +317,17 @@ window.intercomSettings = {
     app_id: 'ymua2wwv'
 };
 (function () { var w = window; var ic = w.Intercom; if (typeof ic === 'function') { ic('reattach_activator'); ic('update', intercomSettings); } else { var d = document; var i = function () { i.c(arguments) }; i.q = []; i.c = function (args) { i.q.push(args) }; w.Intercom = i; function l() { var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://widget.intercom.io/widget/ymua2wwv'; var x = d.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); } if (w.attachEvent) { w.attachEvent('onload', l); } else { w.addEventListener('load', l, false); } } })() 
+
+// Call me back
+var ZCallbackWidgetLinkId = '58c22e72caa2bab7e417ccdd3e42eade';
+var ZCallbackWidgetDomain = 'my.zadarma.com';
+(function () {
+    var lt = document.createElement('script');
+    lt.type = 'text/javascript';
+    lt.charset = 'utf-8';
+    lt.async = true;
+    lt.src = 'https://' + ZCallbackWidgetDomain + '/callbackWidget/js/main.min.js';
+    var sc = document.getElementsByTagName('script')[0];
+    if (sc) sc.parentNode.insertBefore(lt, sc);
+    else document.documentElement.firstChild.appendChild(lt);
+})();
